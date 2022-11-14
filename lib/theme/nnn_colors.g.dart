2e8c885d266bf67d1063b6277da3661d32127302
Parabeac-Nest <admin@parabeac.com>
2022-11-14 21:03:05 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/nnn_colors.g.dart
@@ -0,0 +1,18 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class NnnColors {
+
+static const Color lightOnPrimary = Color(0xff00455b);
+
+static const Color lightPrimary = Color(0xff75deff);
+
+static const Color lightPrimaryContainer = Color(0xff007093);
+
+static const Color lightOnPrimaryContainer = Color(0xff84e2ff);
+}
---
+++ lib/theme/nnn_theme.g.dart
@@ -0,0 +1,15 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';import 'package:nnn/theme/nnn_colors.g.dart';
+
+class NnnTheme {
+
+static const ColorScheme light = ColorScheme.light(onPrimary: NnnColors.lightOnPrimary,primary: NnnColors.lightPrimary,primaryContainer: NnnColors.lightPrimaryContainer,onPrimaryContainer: NnnColors.lightOnPrimaryContainer);
+
+static  ThemeData themeDataLight = ThemeData( colorScheme: light,);
+}
